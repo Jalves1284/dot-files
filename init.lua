cc4bd798591b8e6ce9cd9dbc6f47ebdacca92d0d
Jalves1284 <alves.m.john@gmail.com>
2025-01-21 01:18:33 +0000

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,6 @@
+vim.cmd('syntax on')
+vim.o.number = true         -- Enable absolute line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.tabstop = 4      -- Number of spaces for a tab
+vim.o.shiftwidth = 4   -- Number of spaces for auto-indentation
+vim.o.expandtab = true -- Use spaces instead of tabs
